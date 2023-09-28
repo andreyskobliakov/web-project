6544afc7c68e2d35c6ce2c60d9b233ecfbd8c018
andreyskobliakov <a.skobliakov@gmail.com>
2023-09-29 02:34:32 +0300

    style: Working with themes, working with inputs, working with buttons and their styles

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <header class="bg-gray-800 px-5 py-5">
+      <div class="container flex items-center justify-between mx-0">
+        <div class="flex items-center"> 
+          <ThemeSwitcher/>
+        </div>
+        <div class="flex items-center justify-between"> 
+          <input
+            v-model="themeName"
+            class="border rounded p-1"
+            placeholder="Enter theme name"
+          />
+          <button
+            class="btn btn-sm ml-2"
+            @click="changeTheme(themeName)"
+          >
+            Change Theme
+          </button>
+        </div>
+      </div>
+    </header>
+  </template>
+  
+  <script>
+  import ThemeSwitcher from './ThemeSwitcher.vue';
+  
+  export default {
+    components: {
+      ThemeSwitcher,
+    },
+    data() {
+      return {
+        pageTitle: 'Заголовок вашей страницы',
+        themeName: '', 
+      };
+    },
+    methods: {
+      changeTheme(theme) {
+        if (theme) {
+          document.documentElement.setAttribute("data-theme", theme);
+        }
+      },
+    },
+  };
+  </script>
+  
