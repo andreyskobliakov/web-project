7e85b7ecebe9c9c99c934cc3f4fea3b1d9d17b3f
andreyskobliakov <a.skobliakov@gmail.com>
2023-10-04 00:00:30 +0300

    feat: adding error validation provider and inject

---
+++ src/components/UI/Textarea.vue
@@ -0,0 +1,23 @@
+<template>
+  <textarea :value="value" @input="updateValue" :placeholder="placeholder" class="textarea textarea-bordered "></textarea>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    placeholder: String,
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit("update:value", event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.textarea {
+  @apply px-3 py-2 text-base border rounded-lg focus:outline-none focus:border-stone-950 transition duration-300 ease-in-out;
+}
+</style>
