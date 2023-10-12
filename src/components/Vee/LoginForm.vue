aeef5a179cb2c8f4bf557748fb6d149076db5cab
Andrey Skobliakov <141303771+andreyskobliakov@users.noreply.github.com>
2023-10-12 10:35:00 +0300

    Update LoginForm.vue

--- src/components/Vee/LoginForm.vue
+++ src/components/Vee/LoginForm.vue
@@ -1,6 +1,6 @@
 <template>
   <VeeForm v-slot="{ submitForm, isSubmitting }" :validation-schema="schema" as="div">
-    <form @submit="submitForm" class="max-w-md mx-auto p-6 mt-8 bg-white rounded-lg shadow-lg">
+    <form @submit="submitForm()" class="max-w-md mx-auto p-6 mt-8 bg-white rounded-lg shadow-lg">
       <div class="mb-4">
         <label for="email" class="block text-gray-600">Email:</label>
         <Field name="email" type="email" id="email" class="mt-1 p-2 border rounded-md focus:ring focus:ring-blue-400 w-full" />
@@ -11,9 +11,15 @@
         <Field name="password" type="password" id="password" class="mt-1 p-2 border rounded-md focus:ring focus:ring-blue-400 w-full" />
         <ErrorMessage name="password" class="text-red-500" />
       </div>
-      <button type="submit" class="btn btn-info" :disabled="isSubmitting">
-        {{ isSubmitting ? 'Submitting...' : 'Submit' }}
-      </button>
+      <div class="mb-4">
+        <button
+          type="submit"
+          class="btn btn-info"
+          :disabled="isSubmitting"
+        >
+          {{ isSubmitting ? 'Submitting...' : 'Submit' }}
+        </button>
+      </div>
     </form>
   </VeeForm>
 </template>
@@ -28,22 +34,38 @@ const schema = yup.object({
   password: yup.string().required().min(8),
 });
 
-// Дополнительные улучшения
-
-const messages = {
-  email: {
-    required: "Введите адрес электронной почты",
-    email: "Введите действительный адрес электронной почты",
-  },
-  password: {
-    required: "Введите пароль",
-    min: "Пароль должен быть не менее 8 символов",
-  },
-};
 
 const isLoading = ref(false);
+const errors = ref({});
 
 function submitForm() {
-  // Отправка формы на сервер
+  isLoading.value = true;
+
+  fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: formValues.email,
+      password: formValues.password,
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      
+      if (data.success) {
+        window.location.href = '/';
+      } else {
+       
+        errors.value = data.errors;
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      isLoading.value = false;
+    });
 }
 </script>
